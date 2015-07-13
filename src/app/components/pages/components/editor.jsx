f85d5a4f292bf76e4c3a240193215c4425e01a6d
b4456609 <b4456609@gmail.com>
2015-07-13 20:58:21 +0800

    add youtube

--- src/app/components/pages/components/editor.jsx
+++ src/app/components/pages/components/editor.jsx
@@ -15,6 +15,39 @@ let {
   Colors
 } = Styles;
 
+function generateYoutubeEmbedCode(user_url, start, end) {
+
+//save embed parameter
+  var startPar = '',
+    endPar = '';
+
+// error input hanlder
+  if (start !== 0 && start > 0) {
+    startPar += '&start=' + start;
+  }
+  if (end !== 0 && end > 0 && end > start) {
+    endPar += '&end=' + end;
+  }
+//get input id
+  var videoId = youtube_parser(user_url);
+
+//set embed code
+  var embedCode = '<p><div id="' + videoId + '" class="youtube video-container">' + '<iframe width="560" height="315" src="http://www.youtube.com/embed/' + videoId + '?controls=1&disablekb=1&modestbranding=1&showinfo=0&rel=0' + startPar + endPar + '" frameborder="0" allowfullscreen></iframe>' + '</div></p>';
+
+  return embedCode;
+}
+
+//parse youtube url to id
+function youtube_parser(url) {
+  var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[7].length == 11) {
+    return match[7];
+  } else {
+    alert("Url incorrecta");
+  }
+}
+
 var Editor = React.createClass({
 
   getInitialState: function() {
@@ -43,16 +76,16 @@ var Editor = React.createClass({
       errorContent: {
         color: Colors.red500
       },
-      textFullWidth:{
+      textFullWidth: {
         width: '100%'
       },
-      textHalfWidth:{
+      textHalfWidth: {
         width: '50%'
       },
-      dialogHeader:{
+      dialogHeader: {
         marginTop: 24
       },
-      loadingText:{
+      loadingText: {
         margin: 'auto 0',
         fontSize: 24
       }
@@ -63,12 +96,16 @@ var Editor = React.createClass({
     let styles = this.getStyles();
 
     let standardActions = [
-      { text: '取消' },
-      { text: '送出', onTouchTap: this._handleImgDialogSubmit }
+      {
+        text: '取消'
+      }, {
+        text: '送出',
+        onTouchTap: this._handleImgDialogSubmit
+      }
     ];
 
     return (
-      <Dialog ref="imgDialog" actions={standardActions} title="插入圖片">
+      <Dialog actions={standardActions} ref="imgDialog" title="插入圖片">
         <TextField floatingLabelText="圖片網址" onChange={this._handleImgDialogtChange} ref="imgInput" style={styles.textFullWidth}/>
       </Dialog>
     );
@@ -78,14 +115,17 @@ var Editor = React.createClass({
   getSlideshareDialog: function() {
     let styles = this.getStyles();
 
-
     let standardActions = [
-      { text: '取消' },
-      { text: '送出', onTouchTap: this._handleImgDialogSubmit }
+      {
+        text: '取消'
+      }, {
+        text: '送出',
+        onTouchTap: this._handleImgDialogSubmit
+      }
     ];
 
     return (
-      <Dialog ref="slideshareDialog" actions={standardActions} title="插入Slideshare">
+      <Dialog actions={standardActions} ref="slideshareDialog" title="插入Slideshare">
         <TextField floatingLabelText="Slideshare網址" ref="slideshareInput" style={styles.textFullWidth}/>
         <h3 style={styles.dialogHeader}>進階選項</h3>
         <TextField floatingLabelText="開始頁面" ref="youtubeStartInput" style={styles.textHalfWidth}/>
@@ -99,30 +139,30 @@ var Editor = React.createClass({
     let styles = this.getStyles();
 
     let standardActions = [
-      { text: '取消' },
-      { text: '送出', onTouchTap: this._handleYoutubeDialogSubmit }
+      {
+        text: '取消'
+      }, {
+        text: '送出',
+        onTouchTap: this._handleYoutubeDialogSubmit
+      }
     ];
 
     return (
-      <Dialog ref="youtubeDialog" actions={standardActions} title="插入Youtube">
+      <Dialog actions={standardActions} ref="youtubeDialog" title="插入Youtube">
         <TextField floatingLabelText="Youtube網址" ref="youtubeInput" style={styles.textFullWidth}/>
         <h3 style={styles.dialogHeader}>進階選項</h3>
         <TextField floatingLabelText="開始時間" ref="youtubeStartInput" style={styles.textHalfWidth}/>
-        <TextField floatingLabelText="結束時間"  ref="youtubeEndInput" style={styles.textHalfWidth}/>
+        <TextField floatingLabelText="結束時間" ref="youtubeEndInput" style={styles.textHalfWidth}/>
       </Dialog>
     );
 
   },
 
-  _handleYoutubeDialogSubmit: function () {
-    
-  },
-
-  getLoadingDialog: function () {
+  getLoadingDialog: function() {
     let styles = this.getStyles();
     return (
       <Dialog ref="loadingDialog">
-        <CircularProgress mode="indeterminate" />
+        <CircularProgress mode="indeterminate"/>
         <span style={styles.loadingText}>請稍後</span>
       </Dialog>
     );
@@ -142,29 +182,43 @@ var Editor = React.createClass({
           </div>
           <textarea id="editor"/>
         </div>
-        {imgDialog}
-        {slideshareDialog}
-        {YoutubeDialog}
-        {loadingDialog}
+        {imgDialog} {slideshareDialog} {YoutubeDialog} {loadingDialog}
       </ClearFix>
     );
   },
 
-
-  _onClickImgButton: function () {
+  _onClickImgButton: function() {
     this.refs.imgDialog.show();
+    this.refs.imgInput.focus();
   },
 
-  _onClickYoutubeButton: function () {
+  _onClickYoutubeButton: function() {
     this.refs.youtubeDialog.show();
+    this.refs.youtubeInput.focus();
   },
 
-  _onClickSlideshareButton: function () {
+  _onClickSlideshareButton: function() {
     this.refs.slideshareDialog.show();
+    this.refs.slideshareInput.focus();
   },
 
-  _handleImgDialogtChange: function () {
+  _handleImgDialogtChange: function() {
     this.refs.imgInput.setErrorText('');
+  },
+
+  _handleYoutubeDialogSubmit: function() {
+    let youtubeUrl = this.refs.youtubeInput.getValue().trim();
+    let start = this.refs.youtubeStartInput.getValue().trim();
+    let end = this.refs.youtubeEndInput.getValue().trim();
+
+    if (youtubeUrl === '') {
+      this.refs.youtubeInput.setErrorText('網址不能空白');
+    } else {
+      this.refs.youtubeDialog.dismiss();
+      let code = generateYoutubeEmbedCode(youtubeUrl, start, end);
+      EditorApi.insertContent(code);
+    }
+
   },
 
   _handleImgDialogSubmit: function() {
@@ -172,22 +226,21 @@ var Editor = React.createClass({
     let value = this.refs.imgInput.getValue().trim();
     if (value === '') {
       this.refs.imgInput.setErrorText('網址不能空白');
-    }
-    else{
+    } else {
       this.refs.imgDialog.dismiss();
       this.refs.loadingDialog.show();
-      let success = function (item) {
+      let success = function(item) {
         self.refs.loadingDialog.dismiss();
-        //get file name
-        let filename = item.link.substring(item.link.lastIndexOf('/')+1);
+//get file name
+        let filename = item.link.substring(item.link.lastIndexOf('/') + 1);
         self.state.file.push(filename);
 
-        //img html code
+//img html code
         let img = "<img id='" + item.id + " ' src='" + item.link + "' width='100%' >";
         EditorApi.insertContent(img);
       };
 
-      let fail = function () {
+      let fail = function() {
         self.refs.loadingDialog.dismiss();
 
       };
