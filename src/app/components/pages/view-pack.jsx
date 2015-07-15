819bcf957d9829ad2e66b2ce7d756ddd72581d53
b4456609 <b4456609@gmail.com>
2015-07-15 17:10:15 +0800

    position note btn

--- src/app/components/pages/view-pack.jsx
+++ src/app/components/pages/view-pack.jsx
@@ -3,7 +3,7 @@ let Router = require('react-router');
 var PackStore = require('../../stores/pack-store');
 let EasyLearnActions = require('../../action/easylearn-actions.jsx');
 let VersionInfo = require('./components/version-info.jsx');
-
+let InsetComment = require('material-ui/lib/svg-icons/editor/insert-comment.js');
 let Tooltip = require('../../api/tooltip-api.js');
 
 let Navigation = Router.Navigation;
@@ -25,62 +25,60 @@ function getViewPackState() {
 }
 
 function getSelectionCoords(win) {
-    win = win || window;
-    var doc = win.document;
-    var sel = doc.selection, range, rects, rect;
-    var x = 0, y = 0;
-    if (sel) {
-        if (sel.type != "Control") {
-            range = sel.createRange();
-            range.collapse(true);
-            x = range.boundingLeft;
-            y = range.boundingTop;
-        }
-    } else if (win.getSelection) {
-        sel = win.getSelection();
-        console.log(sel);
-        if(sel.isCollapsed ==  true){
-          x = 0;
-          y = 0;
-        }
-        else if (sel.rangeCount) {
-            range = sel.getRangeAt(0).cloneRange();
-            if (range.getClientRects) {
-
-                rects = range.getClientRects();
-                console.log(rects);
-                if (rects.length > 0) {
-                    rect = range.getClientRects()[0];
-                }
-                x = rect.right;
-                y = rect.top;
-            }
-            else{
-              x = 0;
-              y = 0;
-            }
-
-            // // Fall back to inserting a temporary element
-            // if (x == 0 && y == 0) {
-            //     var span = doc.createElement("span");
-            //     if (span.getClientRects) {
-            //         // Ensure span has dimensions and position by
-            //         // adding a zero-width space character
-            //         span.appendChild( doc.createTextNode("\u200b") );
-            //         range.insertNode(span);
-            //         rect = span.getClientRects()[0];
-            //         x = rect.left;
-            //         y = rect.top;
-            //         var spanParent = span.parentNode;
-            //         spanParent.removeChild(span);
-            //
-            //         // Glue any broken text nodes back together
-            //         spanParent.normalize();
-            //     }
-            // }
-        }
+  win = win || window;
+  var content = document.getElementById('content');
+  var sel = content.selection,
+    range,
+    rects,
+    rect;
+  var x = 0,
+    y = 0;
+  if (sel) {
+    if (sel.type != "Control") {
+      range = sel.createRange();
+      range.collapse(true);
+      x = range.boundingLeft;
+      y = range.boundingTop;
     }
-    return { x: x, y: y };
+  } else if (win.getSelection) {
+    sel = win.getSelection();
+    console.log(sel);
+    if (sel.isCollapsed == true) {
+      return getWindowSize();
+    } else if (sel.rangeCount) {
+      range = sel.getRangeAt(0).cloneRange();
+      if (range.getClientRects) {
+
+        rects = range.getClientRects();
+        console.log(rects);
+        if (rects.length > 0) {
+          rect = range.getClientRects()[0];
+        }
+        x = rect.right;
+        y = rect.top;
+      } else {
+        return getWindowSize();
+
+      }
+    }
+  }
+  else{
+    return getWindowSize();
+  }
+  return {
+    x: x,
+    y: y
+  };
+}
+
+function getWindowSize() {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+  return {
+    x: w -100,
+    y: h -50
+  }
 }
 
 var ViewPack = React.createClass({
@@ -110,11 +108,21 @@ var ViewPack = React.createClass({
     let self = this;
 
 
-    document.getElementById("content").onmouseup = function() {
-        var coords = getSelectionCoords();
-        console.log(coords.x, coords.y);
-        self.setState({x: coords.x, y:coords.y});
+    document.onmouseup = function() {
+      var coords = getSelectionCoords();
+      console.log(coords.x, coords.y);
+      self.setState({
+        x: coords.x,
+        y: coords.y
+      });
     };
+
+    $(document).not("#content").mouseup(function () {
+      self.setState(getWindowSize());
+    });
+
+    self.setState(getWindowSize());
+
   },
 
   componentWillUnmount: function() {
@@ -126,7 +134,7 @@ var ViewPack = React.createClass({
   },
 
   getStyles: function() {
-    return {
+    let styles = {
       contentPaper: {
         marginRight: '324px',
         marginBottom: '16px'
@@ -183,12 +191,22 @@ var ViewPack = React.createClass({
       submitBtn: {
         width: 100
       },
-      floatBtn:{
-        position:'absolute',
-        top: this.state.y + 20,
+      floatBtn: {
+        position: 'absolute',
+        top: this.state.y + 30,
         left: this.state.x + 20
       }
+    };
+
+    let windowSize = getWindowSize();
+
+    if(this.state.y == windowSize.y && this.state.x == windowSize.x) {
+      styles.floatBtn.position = 'fixed';
+      styles.floatBtn.top = this.state.y-50;
+      styles.floatBtn.left = this.state.x-50;
     }
+
+    return styles;
   },
 
   _onClickNote: function(noteText, note) {
@@ -239,33 +257,30 @@ var ViewPack = React.createClass({
         <table>
           <tr>
             <td style={styles.textField}>
-              <TextField style={styles.textField} ref="message"
-                floatingLabelText="留言" multiLine={true} />
+              <TextField floatingLabelText="留言" multiLine={true} ref="message" style={styles.textField}/>
             </td>
             <td style={styles.submitBtn}>
-              <RaisedButton label="送出"           onClick={this._onSubmitMessage} />
+              <RaisedButton label="送出" onClick={this._onSubmitMessage}/>
             </td>
           </tr>
         </table>
       </div>
     )
-
     return (
       <Dialog autoDetectWindowHeight={true} autoScrollBodyContent={true} ref="noteDialog" title={this.state.noteText}>
         <div>
           {content}
           <h3 style={styles.commentHeader}>Comment:</h3>
-          {comment}
-          {userInput}
+          {comment} {userInput}
         </div>
       </Dialog>
     );
   },
 
-  _onSubmitMessage: function () {
+  _onSubmitMessage: function() {
     this.refs.message.setErrorText('');
     let text = this.refs.message.getValue().trim();
-    if(text === ''){
+    if (text === '') {
       this.refs.message.setErrorText('請輸入留言');
       return;
     }
@@ -283,7 +298,7 @@ var ViewPack = React.createClass({
 
           <div style={styles.rightBlock}>
             <ClearFix>
-              <VersionInfo versionInfo={this.state.pack.versionInfo} currentVersionId={this.state.pack.version.id}/>
+              <VersionInfo currentVersionId={this.state.pack.version.id} versionInfo={this.state.pack.versionInfo}/>
             </ClearFix>
           </div>
 
@@ -298,7 +313,9 @@ var ViewPack = React.createClass({
 
         </div>
         {note}
-        <FloatingActionButton iconClassName="muidocs-icon-action-grade" mini={true} style={styles.floatBtn}/>
+        <FloatingActionButton secondary={true} style={styles.floatBtn}>
+          <InsetComment/>
+        </FloatingActionButton>
       </ClearFix>
     );
   }
