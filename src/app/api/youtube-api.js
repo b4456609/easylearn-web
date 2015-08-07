0c6703e1071c7a68403a0054e181f0c2c5135514
b4456609 <b4456609@gmail.com>
2015-08-07 20:06:28 +0800

    fix add youtube

--- src/app/api/youtube-api.js
+++ src/app/api/youtube-api.js
@@ -1,30 +1,8 @@
-function generateYoutubeEmbedCode(user_url, start, end) {
-
-//save embed parameter
-  var startPar = '',
-    endPar = '';
-
-// error input hanlder
-  if (start !== 0 && start > 0) {
-    startPar += '&start=' + start;
-  }
-  if (end !== 0 && end > 0 && end > start) {
-    endPar += '&end=' + end;
-  }
-//get input id
-  var videoId = youtube_parser(user_url);
-
-//set embed code
-  var embedCode = '<p><div id="' + videoId + '" class="youtube video-container">' + '<iframe width="560" height="315" src="http://www.youtube.com/embed/' + videoId + '?controls=1&disablekb=1&modestbranding=1&showinfo=0&rel=0' + startPar + endPar + '" frameborder="0" allowfullscreen></iframe>' + '</div></p>';
-
-  return embedCode;
-}
-
 //parse youtube url to id
 function youtube_parser(url) {
   var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
   var match = url.match(regExp);
-  if (match && match[7].length == 11) {
+  if (match && match[7].length === 11) {
     return match[7];
   } else {
     alert("Url incorrecta");
@@ -32,8 +10,25 @@ function youtube_parser(url) {
 }
 
 let YoutubeApi = {
-  generateYoutubeEmbedCode:function (user_url, start, end) {
-    generateYoutubeEmbedCode(user_url, start, end);
+  generateYoutubeEmbedCode: function(user_url, start, end) {
+    //save embed parameter
+    var startPar = '',
+      endPar = '';
+
+    // error input hanlder
+    if (start !== 0 && start > 0) {
+      startPar += '&start=' + start;
+    }
+    if (end !== 0 && end > 0 && end > start) {
+      endPar += '&end=' + end;
+    }
+    //get input id
+    var videoId = youtube_parser(user_url);
+
+    //set embed code
+    var embedCode = '<p><div id="' + videoId + '" class="youtube video-container">' + '<iframe width="560" height="315" src="http://www.youtube.com/embed/' + videoId + '?controls=1&disablekb=1&modestbranding=1&showinfo=0&rel=0' + startPar + endPar + '" frameborder="0" allowfullscreen></iframe>' + '</div></p>';
+
+    return embedCode;
   }
 }
 
