fe2d940d62ba4b06eb24c480b10e18a34d275083
Bernie <b4456609@gmail.com>
2016-09-19 09:54:55 +0800

    add menu

--- src/page/components/Header.js
+++ src/page/components/Header.js
@@ -1,17 +1,34 @@
 import React from 'react';
 
 
-const Header = ({ title }) => (
+const Header = ({ title, removeFolder }) => (
   <header className="mdl-layout__header">
     <div className="mdl-layout__header-row">
-      <span className="mdl-layout-title">{title || 'All'}</span>
+      <span className="mdl-layout-title">{title || 'Easylearn'}</span>
       <div className="mdl-layout-spacer" />
+      <div>
+        <button
+          id="appbar-menu-lower-right"
+          className="mdl-button mdl-js-button mdl-button--icon"
+        >
+          <i className="material-icons">more_vert</i>
+        </button>
+        <ul
+          className="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+          htmlFor="appbar-menu-lower-right"
+        >
+          <li className="mdl-menu__item" onClick={removeFolder}>
+            Remove Folder
+          </li>
+        </ul>
+      </div>
     </div>
   </header>
 );
 
 Header.propTypes = {
   title: React.PropTypes.string,
+  removeFolder: React.PropTypes.func.isRequired,
 };
 
 export default Header;
