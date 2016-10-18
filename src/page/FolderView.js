e64bad8173987f6c79fd8eee553da433685eb83d
Bernie <b4456609@gmail.com>
2016-10-18 10:27:49 +0800

    add empty folder text add empty folder test

--- src/page/FolderView.js
+++ src/page/FolderView.js
@@ -2,23 +2,40 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PackCard from './components/PackCard';
 
-
 const FolderView = ({ pack, folderId }) => {
-  const packs = (pack.map(
-    i => (
-      <PackCard
-        key={i.id}
-        name={i.name}
-        description={i.description}
-        id={i.id}
-        folderId={folderId}
-      />
-    )
-  ));
-
+  let content;
+  if (pack.length !== 0) {
+    content = (pack.map(
+      i => (
+        <PackCard
+          key={i.id}
+          name={i.name}
+          description={i.description}
+          id={i.id}
+          folderId={folderId}
+        />
+      )
+    ));
+  } else {
+    content = (
+      <div style={{width:'100%',margin:'36px', textAlign: 'center', color:'grey'}}>
+        <i
+          className="material-icons"
+          style={{
+            fontSize: '200px'
+          }}
+        >
+          book
+        </i>
+        <h5>
+          移動懶人包到此資料夾
+        </h5>
+      </div>
+    );
+  }
   return (
     <div className="mdl-grid">
-      {packs}
+      {content}
     </div>
   );
 };
--- src/reducers/folder.js
+++ src/reducers/folder.js
@@ -1,10 +1,10 @@
 import {
-    ADD_FOLDER,
-    NEW_PACK,
-    MOVE_PACK_TO_FOLDER,
-    REMOVE_PACK,
-    MOVE_PACK_OUT,
-    REMOVE_FOLDER,
+  ADD_FOLDER,
+  NEW_PACK,
+  MOVE_PACK_TO_FOLDER,
+  REMOVE_PACK,
+  MOVE_PACK_OUT,
+  REMOVE_FOLDER,
 } from '../actions';
 
 const ALL_FOLDER = 'all';
@@ -14,37 +14,48 @@ const ALL_FOLDER = 'all';
 //   pack: [],
 // };
 
-const initState = [{
-  name: '全部懶人包',
-  id: 'all',
-  pack: [
-    'pack1474100598141',
-  ],
-}];
+const initState = [
+  {
+    name: '全部懶人包',
+    id: 'all',
+    pack: ['pack1474100598141',]
+  }, {
+    'name': 'a',
+    'id': 'folder1476756912004',
+    'pack': [],
+  },
+];
 
 const folder = (state = initState, action) => {
   switch (action.type) {
     case ADD_FOLDER:
-      return [...state, {
-        name: action.name,
-        id: action.id,
-        pack: [],
-      }];
+      return [
+        ...state, {
+          name: action.name,
+          id: action.id,
+          pack: [],
+        },
+      ];
     case NEW_PACK:
       return state.map((i) => {
-        if (i.id !== ALL_FOLDER) return i;
-        return Object.assign({}, i, { pack: [
-          ...i.pack,
-          action.id,
-        ] });
+        if (i.id !== ALL_FOLDER)
+          return i;
+        return Object.assign({}, i, {
+          pack: [
+            ...i.pack,
+            action.id,
+          ]
+        });
       });
     case MOVE_PACK_TO_FOLDER:
       return state.map((i) => {
         if (i.id === action.folderId && i.pack.indexOf(action.packId) === -1) {
-          return Object.assign({}, i, { pack: [
-            ...i.pack,
-            action.packId,
-          ] });
+          return Object.assign({}, i, {
+            pack: [
+              ...i.pack,
+              action.packId,
+            ]
+          });
         }
         return i;
       });
@@ -52,10 +63,12 @@ const folder = (state = initState, action) => {
       return state.map((i) => {
         const index = i.pack.indexOf(action.packId);
         if (index !== -1) {
-          return Object.assign({}, i, { pack: [
-            ...i.pack.slice(0, index),
-            ...i.pack.slice(index + 1),
-          ] });
+          return Object.assign({}, i, {
+            pack: [
+              ...i.pack.slice(0, index),
+              ...i.pack.slice(index + 1),
+            ]
+          });
         }
         return i;
       });
@@ -63,18 +76,21 @@ const folder = (state = initState, action) => {
       return state.map((i) => {
         if (action.folderId === i.id) {
           const index = i.pack.indexOf(action.packId);
-          return Object.assign({}, i, { pack: [
-            ...i.pack.slice(0, index),
-            ...i.pack.slice(index + 1),
-          ] });
+          return Object.assign({}, i, {
+            pack: [
+              ...i.pack.slice(0, index),
+              ...i.pack.slice(index + 1),
+            ]
+          });
         }
         return i;
       });
     case REMOVE_FOLDER:
       const index = state.findIndex(ele => (ele.id === action.folderId));
-      return [...state.slice(0, index),
+      return [
+        ...state.slice(0, index),
         ...state.slice(index + 1),
-        ];
+      ];
     default:
       return state;
   }
