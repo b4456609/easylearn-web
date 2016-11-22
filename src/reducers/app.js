ebcbd2612726414ce48f227744e5cbc988f27a58
Bernie <b4456609@gmail.com>
2016-11-22 08:24:07 +0800

    add folder loading state and loading indicator

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -2,14 +2,18 @@ import {
     USER_NOT_LOGIN,
     USER_FB_LOGIN_SUCCESS,
     APP_LOGIN_SUCCESS,
-    FETCHING,
+    PACK_FETCHING,
     PACK_FETCHING_ERROR,
     SUCCESS_LOAD_PACK,
+    FOLDER_FETCHING,
+    FOLDER_FETCHING_ERROR,
+    SUCCESS_LOAD_FOLDER
 } from '../actions';
 
 let initState = {
   initState: 'APP_LOGIN_SUCCESS',
-  packFetch: 'false'
+  packFetch: false,
+  folderFetch: false,
 };
 
 if (process.env.NODE_ENV === 'production') {
@@ -21,11 +25,17 @@ if (process.env.NODE_ENV === 'production') {
 
 const app = (state = initState, action) => {
   switch (action.type) {
-    case FETCHING:
+    case PACK_FETCHING:
       return Object.assign({}, state, { packFetch: true });
     case SUCCESS_LOAD_PACK:
       return Object.assign({}, state, { packFetch: false });
     case PACK_FETCHING_ERROR:
+      return Object.assign({}, state, { packFetch: 'error' });
+    case FOLDER_FETCHING:
+      return Object.assign({}, state, { packFetch: true });
+    case SUCCESS_LOAD_FOLDER:
+      return Object.assign({}, state, { packFetch: false });
+    case FOLDER_FETCHING_ERROR:
       return Object.assign({}, state, { packFetch: 'error' });
     case USER_NOT_LOGIN:
       return Object.assign({}, state, { initState: USER_NOT_LOGIN });
